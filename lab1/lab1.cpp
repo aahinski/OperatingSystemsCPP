5a9d44bae86ea4514c9da7bfbe6d59759ea73bad
ahinski <ahinski.inc@gmail.com>
2022-02-16 22:37:40 +0300

    lab1

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
-struct my_struct {
-    volatile int* array;
+struct mystruct {
+    volatile long* array;
     int size;
 };
 
-my_struct object;
+mystruct object;
 
 void worker() {
-    int i = 0;
-    for (;i < object.size;) {
+    cout << "\n" << "Numbers in the array divided by four:" << "\n";
+    for (int i = 0; i < object.size; i++) {
         if (object.array[i] % 4 == 0)
-            cout << object.array[i] << ' ';
+            cout << object.array[i] << "\n";
         Sleep(12);
         i++;
     }
@@ -27,24 +27,28 @@ int main() {
     UINT IDThread1;
 
     object.size = 100;
+    cout << "Enter array size:" << "\n";
+    cin >> object.size;
+    cout << "\n";
 
-    volatile int* array = new int[object.size];
-    cout << "Array:" << ' ';
+    volatile long* array = new long[object.size];
+    cout << "Array:" << "\n";
 
     for (int i = 0; i < object.size; i++) {
         array[i] = rand();
-        cout << array[i] << ' ';
+        cout << array[i] << "\n";
     }
 
     object.array = array;
+    cout << "\n";
 
     int worker_suspend_time, worker_restart_time;
-    cout << "Enter worker suspend time";
+    cout << "Enter worker suspend time"; // время работы worker до остановки
     cin >> worker_suspend_time;
-    cout << "Enter worker restart time";
+    cout << "Enter worker resume time"; // время, пока worker "спит" до возобновления
     cin >> worker_restart_time;
 
-    hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)worker, (my_struct*)&object, 0, &IDThread);
+    hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)worker, (mystruct*)&object, 0, &IDThread);
 
     if (hThread == NULL)
         return GetLastError();
@@ -58,18 +62,18 @@ int main() {
     CloseHandle(hThread);
 
 
+    //Запуск потока с использованием _beginthreadex
+    cout << "\n";
+    cout << "-------------------------------------------";
+    cout << "\n";
 
-    cout << "----------------------";
     hThread = (HANDLE)
-        _beginthreadex(NULL, 0, (_beginthreadex_proc_type)worker, (my_struct*)&object, 0, &IDThread1);
+        _beginthreadex(NULL, 0, (_beginthreadex_proc_type)worker, (mystruct*)&object, 0, &IDThread1);
     if (hThread == NULL)
         return GetLastError();
     WaitForSingleObject(hThread, INFINITE);
+    TerminateThread(hThread, 0);
     CloseHandle(hThread);
-    hThread = (HANDLE)
-        _beginthreadex(NULL, 0, (_beginthreadex_proc_type)worker, (my_struct*)&object, 0, &IDThread1);
-    if (hThread == NULL)
-        return GetLastError();
 
     return 0;
 }
